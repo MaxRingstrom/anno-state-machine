d96792b83b296286e6d0abffc3d99023844e70bf
Max Ringström <max.ringstrom@jayway.com>
2017-02-15 15:05:38 +0100

    Testing that a unsatisfied guard doesn't steal the signal

--- app/src/test/java/com/jayway/annostatemachine/basicconnectiontests/BasicConnectionsTests.java
+++ app/src/test/java/com/jayway/annostatemachine/basicconnectiontests/BasicConnectionsTests.java
@@ -7,14 +7,19 @@ import com.jayway.annostatemachine.annotations.Signals;
 import com.jayway.annostatemachine.annotations.StateMachine;
 import com.jayway.annostatemachine.annotations.States;
 import com.jayway.annostatemachine.basicconnectiontests.generated.GuardedMachineImpl;
+import com.jayway.annostatemachine.basicconnectiontests.generated.MultiConnectionsWithSameSignalMachineImpl;
 import com.jayway.annostatemachine.basicconnectiontests.generated.UnguardedMachineImpl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.verification.VerificationMode;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -49,6 +54,24 @@ public class BasicConnectionsTests {
         verify(stateMachine).onStartSignal(Matchers.<SignalPayload>any());
         verify(mMockEventListener, never()).onChangingState(GuardedMachine.State.INITIAL_STATE,
                 GuardedMachine.State.STARTED);
+    }
+
+    @Test
+    public void testBlockingGuardPassesSignalOn() {
+        MultiConnectionsWithSameSignalMachineImpl stateMachine = spy(new MultiConnectionsWithSameSignalMachineImpl());
+        stateMachine.init(MultiConnectionsWithSameSignalMachineImpl.State.INITIAL_STATE, mMockEventListener);
+        stateMachine.send(MultiConnectionsWithSameSignalMachineImpl.Signal.START);
+
+
+        // We also verify that the connections are called in the order that they are declared.
+        // It is up to the developer normally to make sure that two connections for the same signal
+        // do not have a satisfied guard at the same time. If that is the case then the first connection
+        // to be invoked will be run.
+        InOrder inOrder = inOrder(stateMachine, mMockEventListener);
+        inOrder.verify(stateMachine).onStartSignalDontDoAnything(Matchers.<SignalPayload>any());
+        inOrder.verify(stateMachine).onStartSignalShouldRun(Matchers.<SignalPayload>any());
+        inOrder.verify(mMockEventListener).onChangingState(MultiConnectionsWithSameSignalMachineImpl.State.INITIAL_STATE,
+                MultiConnectionsWithSameSignalMachineImpl.State.STARTED);
     }
 
     @StateMachine
@@ -90,6 +113,33 @@ public class BasicConnectionsTests {
             // Return false so the state transition shouldn't be run. This is called a "guard" where
             // returning false means that the guard blocks the request.
             return false;
+        }
+    }
+
+    @StateMachine
+    public static class MultiConnectionsWithSameSignalMachine {
+
+        @Signals
+        public enum Signal {
+            START
+        }
+
+        @States
+        public enum State {
+            INITIAL_STATE,
+            STARTED
+        }
+
+        @Connection(from = "INITIAL_STATE", to = "STARTED", signal = "START")
+        public boolean onStartSignalDontDoAnything(SignalPayload payload) {
+            // Return false indicating that the guard is not satisfied
+            return false;
+        }
+
+        @Connection(from = "INITIAL_STATE", to = "STARTED", signal = "START")
+        public boolean onStartSignalShouldRun(SignalPayload payload) {
+            // Return true indicating that the guard is satisfied
+            return true;
         }
     }
 }
