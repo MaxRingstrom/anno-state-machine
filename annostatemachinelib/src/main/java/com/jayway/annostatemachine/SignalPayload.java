69d514bc1b0023e524677d6c6f75e2ffeccc9dad
Max Ringström <max.ringstrom@jayway.com>
2017-02-15 19:01:39 +0100

    Testing that global connections are called after explicit guard failure

--- annostatemachinelib/src/main/java/com/jayway/annostatemachine/SignalPayload.java
+++ annostatemachinelib/src/main/java/com/jayway/annostatemachine/SignalPayload.java
@@ -4,18 +4,38 @@ import java.util.HashMap;
 
 public class SignalPayload {
     private HashMap<String, Boolean> mBooleanMap;
+    private HashMap<String, Integer> mIntegerMap;
 
-    public void put(String key, Boolean value) {
+    public SignalPayload put(String key, Boolean value) {
         if (mBooleanMap == null) {
             mBooleanMap = new HashMap<>();
         }
         mBooleanMap.put(key, value);
+        return this;
     }
 
-    public boolean getBoolean(String key, Boolean defaultValue) {
+    public SignalPayload put(String key, Integer value) {
+        if(this.mIntegerMap == null) {
+            this.mIntegerMap = new HashMap();
+        }
+
+        this.mIntegerMap.put(key, value);
+        return this;
+    }
+
+    public Boolean getBoolean(String key, Boolean defaultValue) {
         if (mBooleanMap == null) {
             return defaultValue;
         }
-        return mBooleanMap.get(key);
+        Boolean mapValue = mBooleanMap.get(key);
+        return mapValue == null ? defaultValue : mapValue;
+    }
+
+    public Integer getInt(String key, Integer defaultValue) {
+        if (mIntegerMap == null) {
+            return defaultValue;
+        }
+        Integer mapValue = mIntegerMap.get(key);
+        return mapValue == null ? defaultValue : mapValue;
     }
 }
--- app/src/test/java/com/jayway/annostatemachine/globalconnectiontests/GlobalConnectionTests.java
+++ app/src/test/java/com/jayway/annostatemachine/globalconnectiontests/GlobalConnectionTests.java
@@ -6,8 +6,6 @@ import com.jayway.annostatemachine.annotations.Connection;
 import com.jayway.annostatemachine.annotations.Signals;
 import com.jayway.annostatemachine.annotations.StateMachine;
 import com.jayway.annostatemachine.annotations.States;
-import com.jayway.annostatemachine.eavesdroppertests.generated.MixedEavesDropAndNormalConnectionsMachineImpl;
-import com.jayway.annostatemachine.eavesdroppertests.generated.SimpleEavesDroppingMachineImpl;
 import com.jayway.annostatemachine.globalconnectiontests.generated.GlobalMachineImpl;
 
 import org.junit.Test;
@@ -60,7 +58,7 @@ public class GlobalConnectionTests {
 
         // The explicit error handling connection should get called but not the global ones.
         InOrder inOrder = Mockito.inOrder(stateMachine, mMockEventListener);
-        inOrder.verify(stateMachine).onErrorWhenStarted((SignalPayload) any());
+        inOrder.verify(stateMachine).onNon404ErrorWhenStarted((SignalPayload) any());
         inOrder.verify(mMockEventListener).onChangingState(GlobalMachine.State.STARTED, GlobalMachine.State.ERROR_WHEN_STARTED);
 
         verify(stateMachine, never()).onGlobalErrorWithUnsatisfiedGuard(Matchers.<SignalPayload>any());
@@ -69,11 +67,34 @@ public class GlobalConnectionTests {
 
     @Test
     public void testGlobalConnectionsCalledIfExplicitConnectionGuardIsUnsatisfied() {
+        GlobalMachineImpl stateMachine = spy(new GlobalMachineImpl());
+        stateMachine.init(GlobalMachineImpl.State.INITIAL_STATE, mMockEventListener);
 
+        // Move to started state
+        stateMachine.send(GlobalMachineImpl.Signal.START);
+
+        // Send a ERROR signal with code ERROR_CODE_NOT_FOUND that fails the explicit connection's
+        // guard.
+        stateMachine.send(GlobalMachineImpl.Signal.ERROR,
+                new SignalPayload()
+                        .put(GlobalMachine.KEY_ERROR_CODE, GlobalMachine.ERROR_CODE_NOT_FOUND));
+
+        // The explicit error handling connection should get called but its guard is not satisfied.
+        // This should lead to the global ones being called.
+        InOrder inOrder = Mockito.inOrder(stateMachine, mMockEventListener);
+        inOrder.verify(stateMachine).onNon404ErrorWhenStarted((SignalPayload) any());
+        inOrder.verify(stateMachine).onGlobalErrorWithUnsatisfiedGuard((SignalPayload) any());
+        inOrder.verify(stateMachine).onGlobalErrorWithSatisfiedGuard((SignalPayload) any());
+        inOrder.verify(mMockEventListener).onChangingState(GlobalMachine.State.STARTED, GlobalMachine.State.ERROR);
+
+        verify(mMockEventListener, never()).onChangingState(GlobalMachine.State.STARTED, GlobalMachine.State.ERROR_WHEN_STARTED);
     }
 
     @StateMachine
     public static class GlobalMachine {
+
+        public static final String KEY_ERROR_CODE = "error_code";
+        public static final int ERROR_CODE_NOT_FOUND = 404;
 
         @Signals
         public enum Signal {
@@ -97,10 +118,10 @@ public class GlobalConnectionTests {
         }
 
         @Connection(from = "STARTED", to="ERROR_WHEN_STARTED", signal = "ERROR")
-        public boolean onErrorWhenStarted(SignalPayload payload) {
-            // Explicit handling of ERROR signal when in STARTED state. This overrides the general
-            // error handlers.
-            return true;
+        public boolean onNon404ErrorWhenStarted(SignalPayload payload) {
+            // An explicit connection for non ERROR_CODE_NOT_FOUND errors. If a ERROR_CODE_NOT_FOUND
+            // is received the guard will fail.
+            return !(payload != null && payload.getInt(KEY_ERROR_CODE, -1) == ERROR_CODE_NOT_FOUND);
         }
 
         // A global error connection with an unsatisfied guard that should only trigger if the current state does not handle
@@ -118,6 +139,7 @@ public class GlobalConnectionTests {
         public boolean onGlobalErrorWithSatisfiedGuard(SignalPayload payload) {
             return true;
         }
+
 
     }
 }
