633eeb6f301d274ec76b404be8c4eec5a598ae7b
Max Ringström <max.ringstrom@tunstallnordic.com>
2018-03-08 12:12:01 +0100

    React app set up to talk to localhost server

--- anno-state-machine-debugger/client/src/App.js
+++ anno-state-machine-debugger/client/src/App.js
@@ -3,16 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    version: ''
+  };
+
+  componentDidMount() {
+    document.title = "Anno State Machine Debugger";
+    this.callApi()
+      .then(res => this.setState({ version: res.version }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/version');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Anno State Machine Debugger</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-BuildInfo"><p>Backend version: {this.state.version}</p></div>
       </div>
     );
   }
--- anno-state-machine-debugger/server.js
+++ anno-state-machine-debugger/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/version', (req, res) => {
-  res.send({ express: 'Version 0.0.1' });
+  res.send({ version: '0.0.1' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
